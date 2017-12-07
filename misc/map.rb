29201d069a8d62b9b4c7cfdcd39d9bbd563ea6c2
fukata <tatsuya.fukata@gmail.com>
2017-12-08 02:43:43 +0700

    implement plot polyline on google map

--- misc/map.rb
+++ misc/map.rb
@@ -1,7 +1,56 @@
 #!/usr/bin/env ruby
-require Bundler.require
+require 'bundler'
+Bundler.require
+
+require 'active_support'
+require 'active_support/core_ext'
+
+def parse_options
+  options = {}
+  OptionParser.new do |opt|
+    opt.on('--location PATH', '/path/to/location/file') {|v| options[:location] = v }
+    opt.on('--google-api-key KEY', 'YOUR API KEY') {|v| options[:google_api_key] = v }
+    opt.on('--output PATH', '/path/to/output') {|v| options[:output] = v }
+    opt.parse!(ARGV)
+  end
+  options
+end
+
+def parse_location_log_file(path, opt)
+  location_logs = []
+  File.foreach(path) {|line|
+    puts line
+    time_str, json_str = line.split("\t")
+    time = Time.parse(time_str)
+    dat = JSON.parse(json_str, symbolize_names: true)
+    puts "time=#{time}, dat=#{dat}"
+    location_logs.push({time: time, dat: dat})
+  }
+  location_logs
+end
+
+def render_erb(template_path, locals, opt)
+  bind = binding
+  locals.each do |k,v|
+    bind.local_variable_set(k, v)
+  end
+  ERB.new(File.read(template_path), nil, '-').result(bind)
+end
 
 def main
-  puts "MAKE MAP"
+  opt = parse_options()
+  puts opt
+
+  if opt[:location]
+    location_logs = parse_location_log_file(opt[:location], opt)
+    locations = location_logs.map{|r| r[:dat]}
+    html = render_erb('map.html.erb', {locations: locations}, opt)
+    if opt[:output]
+      File.write(opt[:output], html)
+    else
+      puts html
+    end
+  end
 end
+
 main
